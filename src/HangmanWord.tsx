57b11fa03cba8d6f2d51a21f3cdca53dd1fa3714
ardicDemirol <ardc.dmrol@gmail.com>
2025-05-21 03:00:29 +0300

    first commit

---
+++ src/HangmanWord.tsx
@@ -0,0 +1,31 @@
+export function HangmanWord() {
+  const word = "test";
+  const guessedLetters = ["t", "e", "g"];
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        gap: ".25em",
+        fontSize: "6rem",
+        fontWeight: "bold",
+        textTransform: "uppercase",
+        fontFamily: "monospace",
+      }}
+    >
+      {word.split("").map((letter, i) => (
+        <span style={{ borderBottom: ".1em solid black" }} key={i}>
+          <span
+            style={{
+              visibility: guessedLetters.includes(letter)
+                ? "visible"
+                : "hidden",
+            }}
+          >
+            {letter}
+          </span>
+        </span>
+      ))}
+    </div>
+  );
+}
